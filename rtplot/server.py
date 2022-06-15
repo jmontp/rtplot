1cf45afe8726b95c9908275664ed041289d54941
José A. Montes-Pérez <jmontp@umich.edu>
2022-06-15 17:47:05 -0400

    Added windows compatibility for saving plots

--- rtplot/server.py
+++ rtplot/server.py
@@ -162,6 +162,9 @@ def save_current_plot():
     plot_name = datetime.datetime.now()
     total_name = os.path.join(PLOT_SAVE_PATH, str(plot_name).replace(' ','_')+'.parquet')
     
+    #Remove colons from timestamp for windows file name compatibility
+    total_name = total_name.replace(":","-")
+
     #Create the dataframe object so that we can add ifnro about the subplot names
     df = pd.DataFrame(local_storage_buffer[:local_storage_buffer_num_trace,NUM_DATAPOINTS_IN_PLOT:li+1].T,
                       columns=trace_names)
